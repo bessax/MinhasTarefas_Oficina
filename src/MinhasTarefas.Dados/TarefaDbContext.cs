14a430c52537e79c2385e1a2554b691c9ed59513
bessax <andrebessax@gmail.com>
2024-10-29 16:14:50 -0300

    #update: Implementação da classe de contexto; adição do EFCore;Adicionado referência ao projeto de modelos;

---
+++ src/MinhasTarefas.Dados/TarefaDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MinhasTarefas.Modelos;
+
+namespace MinhasTarefas.Dados;
+public class TarefaDbContext:DbContext
+{
+    public TarefaDbContext(DbContextOptions<TarefaDbContext> options) : base(options) { }
+
+    public DbSet<Usuario> Usuarios { get; set; }
+    public DbSet<Tarefa> Tarefas { get; set; }
+    public DbSet<Categoria> Categorias { get; set; }
+    public DbSet<Comentario> Comentarios { get; set; }
+    public DbSet<SubTarefa> SubTarefas { get; set; }
+
+}
